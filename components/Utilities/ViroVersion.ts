77024b41eb2714db68e6bff4a6a2f4a3a35da740
Benito Mota <benmotanoti@gmail.com>
2026-02-01 11:51:24 -0600

    feat: include latest virocore

--- components/Utilities/ViroVersion.ts
+++ components/Utilities/ViroVersion.ts
@@ -1,1 +1,1 @@
-export const VIRO_VERSION = "2.50.2";
+export const VIRO_VERSION = "2.51.0";
--- dist/components/Utilities/ViroVersion.d.ts
+++ dist/components/Utilities/ViroVersion.d.ts
@@ -1,1 +1,1 @@
-export declare const VIRO_VERSION = "2.50.2";
+export declare const VIRO_VERSION = "2.51.0";
